ef6e3076a574690d6f5b29236356fca2e2905f46
cailynsak24 <casakurai@ucsd.edu>
2025-07-15 11:07:32 -0700

    change file name saving for tessilation.gro

--- scripts/tessilation.py
+++ scripts/tessilation.py
@@ -4,7 +4,7 @@ from util import base_path
 import subprocess
 import shutil
 
-systems = [1]
+systems = [2]
 dimensions = {'xsmall': "1 1 1", 'small': "2 1 1"
 , 'medium': "3 1 1", 'large': "4 1 1"}
 
@@ -18,7 +18,7 @@ for sys in systems:
     gro_file_input = system_path/"equil"/"equilibration6.7.gro"
 
     for size, sizes in dimensions.items():
-        gro_file_output = tessilation_folder/f"tessilation_{size}.gro"
+        gro_file_output = tessilation_folder/f"{size}-tessilation.gro"
         if not gro_file_output.exists():
             tessilation_run = f"gmx genconf -f {gro_file_input} -o {gro_file_output} -nbox {sizes} -dist 0 0 0"
             subprocess.run(tessilation_run, shell=True, check=True)
@@ -27,7 +27,7 @@ for sys in systems:
 
         #create copy of top file 
         top_file = system_path / "system.top"
-        tessilation_top = tessilation_folder / f"{size}-system.top"
+        tessilation_top = tessilation_folder / f"system-{size}.top"
         if not tessilation_top.exists():
             shutil.copy(top_file, tessilation_top)
         else:
@@ -37,14 +37,14 @@ for sys in systems:
         with open(gro_file_output, 'r') as gro_file:
             lines = gro_file.readlines()[2:]
         #set all molecule count to 0 
-        dopc_count = 0
+        DPPC_count = 0
         W = 0
         NA = 0
         CL = 0
         #go through each line in the gro file and add to the counts of each molecule type
         for line in lines:
-            if "DOPC" in line:
-                dopc_count += 1
+            if "DPPC" in line:
+                DPPC_count += 1
             elif "W" in line:
                 W +=1
             elif "NA" in line:
@@ -56,11 +56,11 @@ for sys in systems:
             lines = top_file.readlines()
             unchanged_lines = lines[:9]
         with open(tessilation_top, 'w') as top_file:
-            leaflet = int(dopc_count/2)
+            leaflet = int(DPPC_count/2)
             top_file.writelines(unchanged_lines)
-            top_file.write(f"DOPC {leaflet}\n")
-            top_file.write(f"DOPC {leaflet}\n")
+            top_file.write(f"DPPC {leaflet}\n")
+            top_file.write(f"DPPC {leaflet}\n")
             top_file.write(f"W {W}\n")
             top_file.write(f"NA {NA}\n")
             top_file.write(f"CL {CL}\n")
-            print({dopc_count, W, NA, CL})  
+            print({DPPC_count, W, NA, CL})  
