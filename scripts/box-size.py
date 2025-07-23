b1512f2c47704ae20d841eb04f1e41448d13eb7c
cailynsak24 <casakurai@ucsd.edu>
2025-07-23 09:40:58 -0700

    use of pyedr to read edr files

--- scripts/box-size.py
+++ scripts/box-size.py
@@ -7,36 +7,33 @@ import subprocess
 
 import matplotlib.pyplot as plt
 import numpy as np
+import MDAnalysis as mda 
+#turns the edr file into a numpy array
+
 
 systems = [1,2]
 
 for sys in systems:
     #file set-up
-    system_folder = f"system{sys}"
+    system_folder = f"system{sys}-12.5x12.5x25"
     system_path = base_path/system_folder
-    equilibration_folder = f"100bar-compression"
-    equilibration_path = system_path / equilibration_folder
-    edr = equilibration_path / "xlarge-compression-xz-Pcoupled.edr"
+    compression_folder = f"xzPcoupled-compression"
+    equilibration_path = system_path / compression_folder
+    edr = equilibration_path / "large-compression.edr"
     analysis_folder= system_path / f"analysis"
     analysis_folder.mkdir(exist_ok=True)
-    box_size = analysis_folder / "xlarge-xzdimension-box-size-100bar-xz-Pcoupled.xvg"
 
 
-    #reading edr file, to get the size of the box over time
-    read_edr = f"gmx energy -f {edr} -o {box_size}"
-    subprocess.run(read_edr, shell = True, check = True, input = "11\n13\n", text = True)
+    aux = mda.auxiliary.EDR.EDRReader(edr)
+    terms = aux.get_data(["Box-X", "Box-Z"])
 
-    xy = np.loadtxt(analysis_folder/ f"xlarge-xzdimension-box-size-100bar-xz-Pcoupled.xvg", comments = ('#', '@'))
-    step = xy[:, 0]
-    x_length = xy[:, 1]
-    z_length = xy[:, 2]
 
     #create a plot
-    plt.plot(step, x_length)
-    plt.plot(step, z_length)
-    plt.xlabel('Step')
+    plt.plot(terms["Time"],terms["Box-X"])
+    plt.plot(terms["Time"],terms["Box-Z"])
+    plt.xlabel('Time')
     plt.ylabel('dimension (Nm)')
     plt.legend(['x dimension', 'z dimension'])
-    plt.title(f"box x and z dimension for system{sys} at 100bar")
-    plt.savefig(analysis_folder / f"xlarge-xzdimension{sys}-100bar-xz-Pcoupled.png")
+    plt.title(f"box x and z dimension for system{sys} at 200bar")
+    plt.savefig(analysis_folder / f"large-xzdimension{sys}-200bar-xz-Pcoupled.png")
     plt.close()
