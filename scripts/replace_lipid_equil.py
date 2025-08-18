d528949baedc61fb826a918f2973c31cd4ad31a9
cailynsak24 <casakurai@ucsd.edu>
2025-08-18 14:57:04 -0700

    script to run the minimization and equilibration following the swap DOPC for DPPC

--- scripts/replace_lipid_equil.py
+++ scripts/replace_lipid_equil.py
@@ -8,24 +8,31 @@ import MDAnalysis as mda
 
 #inputs
 sys = 1
-vmd_frame = 50
-time = int(vmd_frame*1000*.02)
+
+#use if you are looking directly at the vmd simulation 
+# vmd_frame = 50
+# frame =  int(vmd_frame*1000*.02)
+
+#use if you're pulling from a particular time (ps)
+frame =  int(85620)
 initial_dim = "8x8x25"
 final_dim = "large"
-lipid = "DOPC"
+compression = "3bar"
+lipid = "DPPC"
+sim_time = 200
+ensemble = "NVT"
 
 #setting paths
 script_dir = Path(__file__).resolve().parent
 mdp_path = Path(script_dir.parent /"mdps")
 
-system_folder = f"system{sys}-{initial_dim}-{lipid}"
-system_path = base_path/system_folder
 analysis = util.analysis_path
 analysis_curv_folder = f"curvature_selection"
-system_analysis_folder = f"system{sys}-{initial_dim}-{lipid}"
-system_analysis_time_folder = f"system{sys}-{initial_dim}-{lipid}-{time}ps"
-analysis_curv_dir = analysis/analysis_curv_folder/f"{system_analysis_folder}"/f"{system_analysis_time_folder}"
-
+system_analysis_folder = analysis/analysis_curv_folder/f"{compression}_{sim_time}ns"/f"system{sys}-{initial_dim}-{lipid}-{frame}ps-NVT"
+system_analysis_folder.mkdir(exist_ok = True)
+system_analysis_time_folder = f"system{sys}-{initial_dim}-{lipid}-{frame}ps-{ensemble}"
+analysis_curv_dir = analysis/analysis_curv_folder/f"{compression}_{sim_time}ns"/f"{system_analysis_folder}"/f"{system_analysis_time_folder}"
+analysis_curv_dir.mkdir(exist_ok = True)
 
 
 top_file = analysis_curv_dir / f"{final_dim}-system.top"
@@ -34,15 +41,15 @@ top_file = analysis_curv_dir / f"{final_dim}-system.top"
 equilibration_folder = analysis_curv_dir / f"equil"
 equilibration_folder.mkdir(exist_ok = True)
 equilibration_path = analysis_curv_dir / equilibration_folder
-equilibration_number = 6.2
-gro = analysis_curv_dir / f"minimization6.1.gro"
+equilibration_number = 7.2
+gro = analysis_curv_dir / f"minimization7.1.gro"
 
-while equilibration_number <= 6.7:
+while equilibration_number <= 7.7:
     # Define files 
     equil_mdp = mdp_path / f"step{equilibration_number}_equilibration.mdp"
     tpr_file = equilibration_path / f"equilibration{equilibration_number}.tpr"
     output_file_equil = equilibration_path / f"equilibration{equilibration_number}"
-    if equilibration_number == 6.2: #running 6.2 equilibration step seperately from the other because it does not require a .cpt file
+    if equilibration_number == 7.2: #running 7.2 equilibration step seperately from the other because it does not require a .cpt file
         #create .tpr
         equil_tpr = f" gmx grompp -p {top_file} -f {equil_mdp} -c {gro} -r {gro} -o {tpr_file} -maxwarn 1"
         subprocess.run(equil_tpr, shell = True, check = True)
@@ -56,7 +63,7 @@ while equilibration_number <= 6.7:
 
         equilibration_number += .1
     #runs 6.3 and above equilibration step 
-    if equilibration_number < 6.7: 
+    if equilibration_number < 7.7: 
         #re-define files 
         equil_mdp = mdp_path / f"step{equilibration_number}_equilibration.mdp"
         tpr_file = equilibration_path / f"equilibration{equilibration_number}.tpr"
@@ -77,26 +84,26 @@ while equilibration_number <= 6.7:
 
         equilibration_number = round(equilibration_number + 0.1,1) 
 
-    if equilibration_number == 6.7: 
-        #re-define files 
-        equil_mdp = mdp_path / f"step{equilibration_number}_equilibration.mdp"
-        tpr_file = equilibration_path / f"equilibration{equilibration_number}.tpr"
-        output_file_equil = equilibration_path / f"equilibration{equilibration_number}"
-        previous_equilibration_number = round(equilibration_number - 0.1,1) 
-        cpt_file = equilibration_path / f"equilibration{previous_equilibration_number}.cpt"
+    # if equilibration_number == 7.7: 
+    #     #re-define files 
+    #     equil_mdp = mdp_path / f"step{equilibration_number}_equilibration.mdp"
+    #     tpr_file = equilibration_path / f"equilibration{equilibration_number}.tpr"
+    #     output_file_equil = equilibration_path / f"equilibration{equilibration_number}"
+    #     previous_equilibration_number = round(equilibration_number - 0.1,1) 
+    #     cpt_file = equilibration_path / f"equilibration{previous_equilibration_number}.cpt"
 
-        #create .tpr
-        equil_tpr = f" gmx grompp -p {top_file} -f {equil_mdp} -c {gro} -r {gro} -o {tpr_file} -t {cpt_file} -maxwarn 1"
-        subprocess.run(equil_tpr, shell = True, check = True)
+    #     #create .tpr
+    #     equil_tpr = f" gmx grompp -p {top_file} -f {equil_mdp} -c {gro} -r {gro} -o {tpr_file} -t {cpt_file} -maxwarn 1"
+    #     subprocess.run(equil_tpr, shell = True, check = True)
 
-        #run equilibration
-        equil_mdrun = f" gmx mdrun -nt 8 -update gpu -gpu_id 0  -pin on -v -pinstride 1 -nstlist 100 -deffnm {output_file_equil}"
-        subprocess.run(equil_mdrun, shell = True, check = True)
+    #     #run equilibration
+    #     equil_mdrun = f" gmx mdrun -nt 8 -update gpu -gpu_id 0  -pin on -v -pinstride 1 -nstlist 100 -deffnm {output_file_equil}"
+    #     subprocess.run(equil_mdrun, shell = True, check = True)
 
-        #update gro file 
-        gro = equilibration_path/ f"equilibration{equilibration_number}"
+    #     #update gro file 
+    #     gro = equilibration_path/ f"equilibration{equilibration_number}"
 
-        equilibration_number = round(equilibration_number + 0.1,1) 
+    #     equilibration_number = round(equilibration_number + 0.1,1) 
         
 
     
--- scripts/replace_lipid_min.py
+++ scripts/replace_lipid_min.py
@@ -8,33 +8,43 @@ import MDAnalysis as mda
 
 #setting paths
 sys = 1
-vmd_frame = 50
-time = int(vmd_frame*1000*.02)
+
+#use if you are looking directly at the vmd simulation 
+# vmd_frame = 50
+# frame =  int(vmd_frame*1000*.02)
+
+#use if you're pulling from a particular time (ps)
+frame =  int(85620)
 initial_dim = "8x8x25"
 final_dim = "large"
-lipid = "DOPC"
+compression = "3bar"
+lipid = "DPPC"
+sim_time = 200
+ensemble = "NVT"
+
 
 script_dir = Path(__file__).resolve().parent
 mdp_path = Path(script_dir.parent /"mdps")
-system_folder = f"system{sys}-{initial_dim}-{lipid}"
-system_path = base_path/system_folder
+
 analysis = util.analysis_path
 analysis_curv_folder = f"curvature_selection"
-system_analysis_folder = f"system{sys}-{initial_dim}-{lipid}"
-system_analysis_time_folder = f"system{sys}-{initial_dim}-{lipid}-{time}ps"
-analysis_curv_dir = analysis/analysis_curv_folder/f"{system_analysis_folder}"/f"{system_analysis_time_folder}"
+system_analysis_folder = analysis/analysis_curv_folder/f"{compression}_{sim_time}ns"/f"system{sys}-{initial_dim}-{lipid}-{frame}ps-NVT"
+system_analysis_folder.mkdir(exist_ok = True)
+system_analysis_time_folder = f"system{sys}-{initial_dim}-{lipid}-{frame}ps-{ensemble}"
+analysis_curv_dir = analysis/analysis_curv_folder/f"{compression}_{sim_time}ns"/f"{system_analysis_folder}"/f"{system_analysis_time_folder}"
+analysis_curv_dir.mkdir(exist_ok = True)
 
 #creates a new .tpr file with DPPC instead of DOPC 
-minimization_number = 6.0 
+minimization_number = 7.0 
 
-gro_file = analysis_curv_dir / f"{final_dim}-compression-{time}ps.gro"
+gro_file = analysis_curv_dir / f"{final_dim}_compression_{frame}ps.gro"
 top_file = analysis_curv_dir / f"{final_dim}-system.top"
 tpr_file = analysis_curv_dir / f"minimization{minimization_number}.tpr"
 mdp_file_min1 = mdp_path / f"step{minimization_number}_minimization.mdp"
 output_file_min1 = analysis_curv_dir / f"minimization{minimization_number}"
 
 
-#need to have restrains for the lowerleaflet PO4
+#tpr for first minimization step 
 minimize1_tpr = f"gmx grompp -f {mdp_file_min1} -c {gro_file} -r {gro_file} -p {top_file} -o {tpr_file}"
 subprocess.run(minimize1_tpr, shell=True, check=True)
 
@@ -45,7 +55,7 @@ subprocess.run(minimize1_run, shell=True, check=True)
 # Run the second minimization step if there is no error
 minimization_number += 0.1
 mdp_file_min2 = mdp_path / f"step{minimization_number}_minimization.mdp"
-gro_file_min1 = analysis_curv_dir / f"minimization6.0.gro"
+gro_file_min1 = analysis_curv_dir / f"minimization7.0.gro"
 output_file_min2 = analysis_curv_dir / f"minimization{minimization_number}"
 tpr_file_min2 = analysis_curv_dir / f"minimization{minimization_number}.tpr"
 
