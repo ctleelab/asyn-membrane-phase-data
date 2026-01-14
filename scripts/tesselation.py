615e13ed07ce1c1d82c679a2ee20aa2c08989695
cailynsak24 <casakurai@ucsd.edu>
2026-01-14 15:21:44 -0800

    tesselation.py generates correct .top file

--- scripts/tesselation.py
+++ scripts/tesselation.py
@@ -9,9 +9,9 @@ from util import base_path
 import subprocess
 import shutil
 
-systems = [1,2,3,4,5,6]
+systems = [3]
 #dimensions = {'xsmall': "1 1 1", 'small': "2 1 1", 'medium': "3 1 1", 'large': "4 1 1", 'xlarge': "10 1 1"}
-dimensions = {'medium': "3 1 1"}
+dimensions = {'large': "4 1 1"}
 lipids = ["DOPC", "DOPS", "DOPA", "DPPC", "DPPS", "DPPA"]
 ions = ["W", "CL", "NA"]
 
@@ -19,9 +19,9 @@ ions = ["W", "CL", "NA"]
 for sys in systems:
     system_folder = f"system{sys}-8x8x25"
     system_path = base_path/system_folder
-    tessilation_folder = system_path / "tessilation"
+    tessilation_folder = system_path / "tessellation"
     tessilation_folder.mkdir(exist_ok =True)
-    gro_file_input = system_path/"equil"/"equilibration6.7.gro"
+    gro_file_input = system_path/"equil-xsmall"/"equilibration6.7.gro"
 
     for size, sizes in dimensions.items():
         gro_file_output = tessilation_folder/f"{size}-tessilation.gro"
@@ -32,7 +32,7 @@ for sys in systems:
             print(f"{gro_file_output} already exists.")
 
         #create copy of top file  
-        top_file = system_path / "system.top"
+        top_file = system_path /"min-xsmall" /"system.top"
         tessilation_top = tessilation_folder / f"{size}-system.top"
         if not tessilation_top.exists():
             shutil.copy(top_file, tessilation_top)
@@ -44,12 +44,6 @@ for sys in systems:
             lines = gro_file.readlines()[2:]
         
         #set all molecules counts as 0
-        DOPC_count = 0
-        DPPC_count = 0
-        DOPA_count = 0
-        DPPA_count = 0 
-        DOPS_count = 0
-        DPPS_count = 0
         W = 0
         NA = 0
         CL = 0
@@ -113,146 +107,181 @@ for sys in systems:
         with open(tessilation_top, 'w') as top_file:
             if size == 'xsmall':
                 top_file.writelines(unchanged_lines)
-            
+                tessellations = 1
+                seperated_DOPC = DOPC_count/tessellations
+                seperated_DOPS = DOPS_count/tessellations
+                seperated_DOPA = DOPA_count/tessellations
+                seperated_DPPC = DPPC_count/tessellations
+                seperated_DPPS = DPPS_count/tessellations
+                seperated_DPPA = DPPA_count/tessellations
+                seperated_W = W/tessellations
+                seperated_NA = NA/tessellations
+                seperated_CL = CL/tessellations
                 # lipids (only write if present)
                 if DOPC_count > 0:
-                    top_file.write(f"DOPC {DOPC_count}\n")
+                    top_file.write(f"DOPC {seperated_DOPC}\n")
                 if DOPS_count > 0:
-                    top_file.write(f"DOPS {DOPS_count}\n")
+                    top_file.write(f"DOPS {seperated_DOPS}\n")
                 if DOPA_count > 0:
-                    top_file.write(f"DOPA {DOPA_count}\n")
+                    top_file.write(f"DOPA {seperated_DOPA}\n")
                 if DPPC_count > 0:
-                    top_file.write(f"DPPC {DPPC_count}\n")
+                    top_file.write(f"DPPC {seperated_DPPC}\n")
                 if DPPS_count > 0:
-                    top_file.write(f"DPPS {DPPS_count}\n")
+                    top_file.write(f"DPPS {seperated_DPPS}\n")
                 if DPPA_count > 0:
-                    top_file.write(f"DPPA {DPPA_count}\n")
+                    top_file.write(f"DPPA {seperated_DPPA}\n")
+                top_file.write(f"W {seperated_W}\n")
+                top_file.write(f"NA {seperated_NA}\n")
+                top_file.write(f"CL {seperated_CL}\n")
 
-                top_file.write(f"W {W}\n")
-                top_file.write(f"NA {NA}\n")
-                top_file.write(f"CL {CL}\n")
 
             elif size == 'small':
-                tessilations = 2
-                seperated_lipid = int(DOPC_count/tessilations)
-                seperated_water = int(W/tessilations)
-                seperated_na = int(NA/tessilations)
-                seperated_cl = int(CL/tessilations)
+                tessellations = 2
+                
+                seperated_DOPC = (DOPC_count/tessellations)/2 #divide by two because two leaflets
+                seperated_DOPS = (DOPS_count/tessellations)/2
+                seperated_DOPA = (DOPA_count/tessellations)/2
+                seperated_DPPC = (DPPC_count/tessellations)/2
+                seperated_DPPS = (DPPS_count/tessellations)/2
+                seperated_DPPA = (DPPA_count/tessellations)/2
+                seperated_W = W/tessellations
+                seperated_NA = NA/tessellations
+                seperated_CL = CL/tessellations
                 top_file.writelines(unchanged_lines)
-                for i in range(tessilations):
-                # lipids (only write if present)
+                for i in range(tessellations):
                     if DOPC_count > 0:
-                        top_file.write(f"DOPC {DOPC_count}\n")
+                        top_file.write(f"DOPC {seperated_DOPC}\n")
                     if DOPS_count > 0:
-                        top_file.write(f"DOPS {DOPS_count}\n")
+                        top_file.write(f"DOPS {seperated_DOPS}\n")
                     if DOPA_count > 0:
-                        top_file.write(f"DOPA {DOPA_count}\n")
+                        top_file.write(f"DOPA {seperated_DOPA}\n")
                     if DPPC_count > 0:
-                        top_file.write(f"DPPC {DPPC_count}\n")
+                        top_file.write(f"DPPC {seperated_DPPC}\n")
                     if DPPS_count > 0:
-                        top_file.write(f"DPPS {DPPS_count}\n")
+                        top_file.write(f"DPPS {seperated_DPPS}\n")
                     if DPPA_count > 0:
-                        top_file.write(f"DPPA {DPPA_count}\n")
+                        top_file.write(f"DPPA {seperated_DPPA}\n")
 
-                    top_file.write(f"W {W}\n")
-                    top_file.write(f"NA {NA}\n")
-                    top_file.write(f"CL {CL}\n")
+                    top_file.write(f"W {seperated_W}\n")
+                    top_file.write(f"NA {seperated_NA}\n")
+                    top_file.write(f"CL {seperated_CL}\n")
 
             elif size == 'medium':
-                tessilations = 3
-                seperated_lipid = int(DOPC_count/tessilations)
-                seperated_water = int(W/tessilations)
-                seperated_na = int(NA/tessilations)
-                seperated_cl = int(CL/tessilations)
+                tessellations = 3
+                seperated_DOPC = (DOPC_count/tessellations)/2 #divide by two because two leaflets
+                seperated_DOPS = (DOPS_count/tessellations)/2
+                seperated_DOPA = (DOPA_count/tessellations)/2
+                seperated_DPPC = (DPPC_count/tessellations)/2
+                seperated_DPPS = (DPPS_count/tessellations)/2
+                seperated_DPPA = (DPPA_count/tessellations)/2
+                seperated_W = W/tessellations
+                seperated_NA = NA/tessellations
+                seperated_CL = CL/tessellations
                 top_file.writelines(unchanged_lines)
-                for i in range(tessilations):
+                for i in range(tessellations):
                     if DOPC_count > 0:
-                        top_file.write(f"DOPC {DOPC_count}\n")
+                        top_file.write(f"DOPC {seperated_DOPC}\n")
+                        print(seperated_DOPC)
                     if DOPS_count > 0:
-                        top_file.write(f"DOPS {DOPS_count}\n")
+                        top_file.write(f"DOPS {seperated_DOPS}\n")
+                        print(seperated_DOPS)
                     if DOPA_count > 0:
-                        top_file.write(f"DOPA {DOPA_count}\n")
+                        top_file.write(f"DOPA {seperated_DOPA}\n")
                     if DPPC_count > 0:
-                        top_file.write(f"DPPC {DPPC_count}\n")
+                        top_file.write(f"DPPC {seperated_DPPC}\n")
                     if DPPS_count > 0:
-                        top_file.write(f"DPPS {DPPS_count}\n")
+                        top_file.write(f"DPPS {seperated_DPPS}\n")
                     if DPPA_count > 0:
-                        top_file.write(f"DPPA {DPPA_count}\n")
+                        top_file.write(f"DPPA {seperated_DPPA}\n")
 
-                    top_file.write(f"W {W}\n")
-                    top_file.write(f"NA {NA}\n")
-                    top_file.write(f"CL {CL}\n")
+                    top_file.write(f"W {seperated_W}\n")
+                    top_file.write(f"NA {seperated_NA}\n")
+                    top_file.write(f"CL {seperated_CL}\n")
 
             elif size == 'large':
-                tessilations = 4
-                seperated_lipid = int(DOPC_count/tessilations)
-                seperated_water = int(W/tessilations)
-                seperated_na = int(NA/tessilations)
-                seperated_cl = int(CL/tessilations)
+                tessellations = 4
+                seperated_DOPC = (DOPC_count/tessellations)/2 #divide by two because two leaflets
+                seperated_DOPS = (DOPS_count/tessellations)/2
+                seperated_DOPA = (DOPA_count/tessellations)/2
+                seperated_DPPC = (DPPC_count/tessellations)/2
+                seperated_DPPS = (DPPS_count/tessellations)/2
+                seperated_DPPA = (DPPA_count/tessellations)/2
+                seperated_W = W/tessellations
+                seperated_NA = NA/tessellations
+                seperated_CL = CL/tessellations
                 top_file.writelines(unchanged_lines)
-                for i in range(tessilations):
+                for i in range(tessellations):
                     if DOPC_count > 0:
-                        top_file.write(f"DOPC {DOPC_count}\n")
+                        top_file.write(f"DOPC {seperated_DOPC}\n")
                     if DOPS_count > 0:
-                        top_file.write(f"DOPS {DOPS_count}\n")
+                        top_file.write(f"DOPS {seperated_DOPS}\n")
                     if DOPA_count > 0:
-                        top_file.write(f"DOPA {DOPA_count}\n")
+                        top_file.write(f"DOPA {seperated_DOPA}\n")
                     if DPPC_count > 0:
-                        top_file.write(f"DPPC {DPPC_count}\n")
+                        top_file.write(f"DPPC {seperated_DPPC}\n")
                     if DPPS_count > 0:
-                        top_file.write(f"DPPS {DPPS_count}\n")
+                        top_file.write(f"DPPS {seperated_DPPS}\n")
                     if DPPA_count > 0:
-                        top_file.write(f"DPPA {DPPA_count}\n")
+                        top_file.write(f"DPPA {seperated_DPPA}\n")
 
-                    top_file.write(f"W {W}\n")
-                    top_file.write(f"NA {NA}\n")
-                    top_file.write(f"CL {CL}\n")
+                    top_file.write(f"W {seperated_W}\n")
+                    top_file.write(f"NA {seperated_NA}\n")
+                    top_file.write(f"CL {seperated_CL}\n")
             elif size == 'xlarge-z' :
-                tessilations = 50
-                seperated_lipid = int(DOPC_count/tessilations)
-                seperated_water = int(W/tessilations)
-                seperated_na = int(NA/tessilations)
-                seperated_cl = int(CL/tessilations)
+                tessellations = 50
+                seperated_DOPC = (DOPC_count/tessellations)/2 #divide by two because two leaflets
+                seperated_DOPS = (DOPS_count/tessellations)/2
+                seperated_DOPA = (DOPA_count/tessellations)/2
+                seperated_DPPC = (DPPC_count/tessellations)/2
+                seperated_DPPS = (DPPS_count/tessellations)/2
+                seperated_DPPA = (DPPA_count/tessellations)/2
+                seperated_W = W/tessellations
+                seperated_NA = NA/tessellations
+                seperated_CL = CL/tessellations
                 top_file.writelines(unchanged_lines)
-                for i in range(tessilations):
+                for i in range(tessellations):
                     if DOPC_count > 0:
-                        top_file.write(f"DOPC {DOPC_count}\n")
+                        top_file.write(f"DOPC {seperated_DOPC}\n")
                     if DOPS_count > 0:
-                        top_file.write(f"DOPS {DOPS_count}\n")
+                        top_file.write(f"DOPS {seperated_DOPS}\n")
                     if DOPA_count > 0:
-                        top_file.write(f"DOPA {DOPA_count}\n")
+                        top_file.write(f"DOPA {seperated_DOPA}\n")
                     if DPPC_count > 0:
-                        top_file.write(f"DPPC {DPPC_count}\n")
+                        top_file.write(f"DPPC {seperated_DPPC}\n")
                     if DPPS_count > 0:
-                        top_file.write(f"DPPS {DPPS_count}\n")
+                        top_file.write(f"DPPS {seperated_DPPS}\n")
                     if DPPA_count > 0:
-                        top_file.write(f"DPPA {DPPA_count}\n")
+                        top_file.write(f"DPPA {seperated_DPPA}\n")
 
-                    top_file.write(f"W {W}\n")
-                    top_file.write(f"NA {NA}\n")
-                    top_file.write(f"CL {CL}\n")
+                    top_file.write(f"W {seperated_W}\n")
+                    top_file.write(f"NA {seperated_NA}\n")
+                    top_file.write(f"CL {seperated_CL}\n")
             elif size == 'xlarge' :
-                tessilations = 10
-                seperated_lipid = int(DOPC_count/tessilations)
-                seperated_water = int(W/tessilations)
-                seperated_na = int(NA/tessilations)
-                seperated_cl = int(CL/tessilations)
+                tessellations = 10
+                seperated_DOPC = (DOPC_count/tessellations)/2 #divide by two because two leaflets
+                seperated_DOPS = (DOPS_count/tessellations)/2
+                seperated_DOPA = (DOPA_count/tessellations)/2
+                seperated_DPPC = (DPPC_count/tessellations)/2
+                seperated_DPPS = (DPPS_count/tessellations)/2
+                seperated_DPPA = (DPPA_count/tessellations)/2
+                seperated_W = W/tessellations
+                seperated_NA = NA/tessellations
+                seperated_CL = CL/tessellations
                 top_file.writelines(unchanged_lines)
-                for i in range(tessilations):
+                for i in range(tessellations):
                     if DOPC_count > 0:
-                        top_file.write(f"DOPC {DOPC_count}\n")
+                        top_file.write(f"DOPC {seperated_DOPC}\n")
                     if DOPS_count > 0:
-                        top_file.write(f"DOPS {DOPS_count}\n")
+                        top_file.write(f"DOPS {seperated_DOPS}\n")
                     if DOPA_count > 0:
-                        top_file.write(f"DOPA {DOPA_count}\n")
+                        top_file.write(f"DOPA {seperated_DOPA}\n")
                     if DPPC_count > 0:
-                        top_file.write(f"DPPC {DPPC_count}\n")
+                        top_file.write(f"DPPC {seperated_DPPC}\n")
                     if DPPS_count > 0:
-                        top_file.write(f"DPPS {DPPS_count}\n")
+                        top_file.write(f"DPPS {seperated_DPPS}\n")
                     if DPPA_count > 0:
-                        top_file.write(f"DPPA {DPPA_count}\n")
+                        top_file.write(f"DPPA {seperated_DPPA}\n")
 
-                    top_file.write(f"W {W}\n")
-                    top_file.write(f"NA {NA}\n")
-                    top_file.write(f"CL {CL}\n")
-                    
+                    top_file.write(f"W {seperated_W}\n")
+                    top_file.write(f"NA {seperated_NA}\n")
+                    top_file.write(f"CL {seperated_CL}\n")
