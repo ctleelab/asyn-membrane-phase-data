91b041244568967332b545c64285f8111545e113
cailynsak24 <casakurai@ucsd.edu>
2026-02-11 23:37:11 -0800

    production extension scripts

---
+++ scripts/production-buckled-systems-ext.py
@@ -0,0 +1,53 @@
+# gmx grommp to take .gro and .top from DOPC 3480ps to DPPC
+
+import util
+from util import base_path
+from pathlib import Path
+import subprocess
+import MDAnalysis as mda
+
+# inputs
+final_dim = "8x8x25"
+strain = "0.2strain"
+systems = [
+    f"system1-{final_dim}-DOPC-{strain}-NVT",
+    f"system2-{final_dim}-DPPC-{strain}-NVT",
+    f"system3-{final_dim}-DOPC-DOPS-{strain}-NVT",
+    f"system4-{final_dim}-DPPC-DPPS-{strain}-NVT",
+    f"system5-{final_dim}-DPPC-DPPA-{strain}-NVT",
+    f"system6-{final_dim}-DOPC-DOPA-{strain}-NVT",
+]
+ensemble = "NVT"
+size = "large"
+
+# setting paths
+script_dir = Path(__file__).resolve().parent
+mdp_path = Path(script_dir.parent / "mdps")
+
+analysis = util.analysis_path
+analysis_curv_folder = f"curvature_selection"
+
+for system in systems:
+    system_analysis_folder = (
+        analysis / analysis_curv_folder / ensemble / "buckled" / system
+    )
+    production_folder = system_analysis_folder / "prod"
+    top_file = system_analysis_folder / f"{size}-system.top"
+   
+    production_number = 7.8
+
+    gro = production_folder  / f"production7.7.gro"
+
+    # Define files
+    equil_mdp = mdp_path / f"step{production_number}_production.mdp"
+    tpr_file = production_folder / f"production{production_number}.tpr"
+    output_file_equil = production_folder / f"production{production_number}"
+    cpt_file = production_folder / f"production7.7.cpt"
+
+    # create .tpr
+    equil_tpr = f" gmx grompp -p {top_file} -f {equil_mdp} -c {gro} -r {gro} -o {tpr_file} -t {cpt_file} -maxwarn 1"
+    subprocess.run(equil_tpr, shell=True, check=True)
+
+    # run production
+    equil_mdrun = f" gmx mdrun -nt 64  -update gpu -gpu_id 0  -pin off -v -pinstride 1 -nstlist 100 -deffnm {output_file_equil}"
+    subprocess.run(equil_mdrun, shell=True, check=True)
