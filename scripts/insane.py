47f6c2f7e2b841bb60a9e9679b66c5e9f4d10fae
cailynsak24 <casakurai@ucsd.edu>
2026-04-10 16:11:21 -0700

    cleaned scripts

--- scripts/insane.py
+++ scripts/insane.py
@@ -1,40 +1,23 @@
-################
-##GENERAL INFO##
-################
-#size of system can be changed, must change folder name and insane_run 
+#helps create the initial system (8x8x25nm) that later gets tessellated to generate the larger systems. 
 
-#load in the systems 
 import util as util
 from util import base_path
-
-#create a folder
 from pathlib import Path
-
-#helps run bash commands within python 
 import subprocess 
 
-
-#goal loop through each system composition in util 
-#create a for loop takes the component system compositions and iterates through the dictionary 
-
 #defining script directory
-#_file_ is the path where the python script is currently being exectuted
-#.resolve gives you the absolute path 
-#.parent takes you to the directory where the script is contained 
 script_dir = Path(__file__).resolve().parent
-#goes up one folder from the parent, and then into martini3-files
 martini_itp_dir = script_dir.parent / "martini3-files"
 
-# #stating completed systems
-completed_systems = [1,2]
+#stating completed systems
+systems = [1,2,3,4,5,6]
 
-#items because system compositions are a dictionary
 for sims, compositions in util.system_compositions.items():
-    if sims not in completed_systems:
+    if sims not in systems:
        continue 
     
     #create a folder for each system in the systems folder
-    folder_name = f"system{sims}-10x10x20"
+    folder_name = f"system{sims}-8x8x25/min-xsmall"
     #coverts the folder name which is a string to a Path object 
     folder_path = base_path/folder_name
     #creates the folder 
@@ -45,9 +28,9 @@ for sims, compositions in util.system_compositions.items():
         compositions_str = f"{k}:{v}"
     
     #write gro and top files to the created folder with no neutralization
-    output_gro = folder_path / f"system-{sims}-6nm.gro"
+    output_gro = folder_path / f"system-{sims}-8nm.gro"
     output_top = folder_path / f"system.top"
-    output_tpr = folder_path / f"system-{sims}-6nm.tpr"
+    output_tpr = folder_path / f"system-{sims}-8nm.tpr"
 
 
     #write gro and top files to the created folder 
@@ -55,7 +38,7 @@ for sims, compositions in util.system_compositions.items():
     neutral_gro = folder_path / f"neutral.gro"
 
     #run insane function 
-    insane_run = f"insane -x 10 -y 10 -z 20 -sol W -o {output_gro} -l {compositions_str} -p {output_top}"
+    insane_run = f"insane -x 8 -y 8 -z 25 -sol W -o {output_gro} -l {compositions_str} -p {output_top}"
 
     #subprocess running, meaning takes the insane_run string and runs it as a bash command
     subprocess.run(insane_run ,shell = True, check = True )
@@ -82,7 +65,7 @@ for sims, compositions in util.system_compositions.items():
     
     subprocess.run(initial_run, shell=True, check=True)
 
-    #neutralize the membrane with NaCl
+    #neutralize the membrane with .15 M NaCl
     neutralize_run = f"gmx genion -s {output_tpr} -o {neutral_gro} -p {output_top} -neutral -conc .15"
 
     #input ensures that Waters is replaced by Na and Cl ions
