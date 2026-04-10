126cba0f404dd63d1e0d1770dbbb794d241d6316
cailynsak24 <casakurai@ucsd.edu>
2026-04-10 11:45:43 -0700

    renamed scripts

--- scripts/equilibration-tessellated-system.py
+++ scripts/equilibration-tessellated-system.py
@@ -24,11 +24,10 @@ for sys in systems:
     equilibration_folder = system_path / f"equil-{size}"
     equilibration_folder.mkdir(exist_ok = True)
     equilibration_path = system_path / equilibration_folder
-    equilibration_number = 6.9
-    #previous_equilibration_number = round(equilibration_number - 0.1,1) 
-    #gro = system_path/ f"min-{size}"/f"minimization6.1.gro"
-    gro = equilibration_path/f"equilibration{equilibration_number}.gro" #set if only running extentensions of equil 
-
+    equilibration_number = 6.2
+    previous_equilibration_number = round(equilibration_number - 0.1,1) 
+    gro = system_path/ f"min-{size}"/f"minimization6.1.gro"
+    #gro = equilibration_path/f"equilibration{previous_equilibration_number}.gro" #set if only running extentensions of equil 
     tesselation_folder = system_path/ "tessellation"
 
     while equilibration_number <= 6.9:
@@ -47,11 +46,12 @@ for sys in systems:
             subprocess.run(equil_mdrun, shell = True, check = True)
 
             #input the equilibration gro file as new gro
-            gro = equilibration_path / f"equilibration{equilibration_number}.gro"
+            gro = equilibration_path / f"equilibration{equilibration_number}"
 
             equilibration_number += .1
-        #runs 6.3 and above equilibration step 
-        if equilibration_number < 6.7: 
+
+        #runs 6.3 - 6.6 equilibration steps
+        if equilibration_number <= 6.7: 
             #re-define files 
             equil_mdp = mdp_path / f"step{equilibration_number}_equilibration.mdp"
             tpr_file = equilibration_path / f"equilibration{equilibration_number}.tpr"
@@ -68,30 +68,33 @@ for sys in systems:
             subprocess.run(equil_mdrun, shell = True, check = True)
 
             #update gro file 
-            gro = equilibration_path/ f"equilibration{equilibration_number}.gro"
+            gro = equilibration_path/ f"equilibration{equilibration_number}"
 
             equilibration_number = round(equilibration_number + 0.1,1) 
 
-        if equilibration_number >= 6.7: 
+        #these run the longer equilibration steps 
+        if equilibration_number >= 6.8: 
+            
             #re-define files 
             equil_mdp = mdp_path / f"step{equilibration_number}_equilibration.mdp"
-            tpr_file = equilibration_path / f"equilibration{equilibration_number}-ext.tpr"
-            output_file_equil = equilibration_path / f"equilibration{equilibration_number}-ext"
-            #previous_equilibration_number = round(equilibration_number - 0.1,1) 
-            cpt_file = equilibration_path / f"equilibration{equilibration_number}.cpt"
-        
+            tpr_file = equilibration_path / f"equilibration{equilibration_number}.tpr"
+            output_file_equil = equilibration_path / f"equilibration{equilibration_number}"
+            previous_equilibration_number = round(equilibration_number - 0.1,1) 
+            cpt_file = equilibration_path / f"equilibration{previous_equilibration_number}.cpt"
+
             #create .tpr
             equil_tpr = f" gmx grompp -p {system_top} -f {equil_mdp} -c {gro} -r {gro} -o {tpr_file} -t {cpt_file} -maxwarn 1"
             subprocess.run(equil_tpr, shell = True, check = True)
 
             #run equilibration
-            equil_mdrun = f" gmx mdrun -nt 32 -bonded gpu -gpu_id 1 -pin on -pinoffset 0 -v -pinstride 1 -nstlist 100 -deffnm {output_file_equil}"
+            equil_mdrun = f" gmx mdrun -nt 32 -bonded gpu -gpu_id 0  -pin on -pinoffset 80  -v -pinstride 1 -nstlist 100 -deffnm {output_file_equil}"
             subprocess.run(equil_mdrun, shell = True, check = True)
 
             #update gro file 
-            gro = equilibration_path/ f"equilibration{equilibration_number}.gro"
+            gro = equilibration_path/ f"equilibration{equilibration_number}"
 
             equilibration_number = round(equilibration_number + 0.1,1) 
+
             
     
         
